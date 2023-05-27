486be7c7dab36f2ffdf8e0fcf08e6ae6a7fcd324
Shahnab <ahmedshahnab@gmail.com>
2023-05-27 10:17:17 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st. set_page_config(layout="wide")
+
+# embed streamlit docs in a streamlit app
+st.components.v1.iframe("https://beta.clamy.app/link/254a1a5bbdb9d1b5", width=1500, height=800, scrolling=True)
+
